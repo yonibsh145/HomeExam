e2009f005dd61c09abeaa9e7dfeca901f5a85db1
Hadar Geva <hadarge@wix.com>
2021-01-20 16:22:54 +0200

    pin button style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,7 +64,7 @@ export class App extends React.PureComponent<{}, AppState> {
 
 		return (<ul className='tickets'>
           {filteredTickets.map((ticket) => (<li key={ticket.id + pinnedView } className='ticket'>
-            <button onClick={()=>{
+            <button className='pinButton' onClick={()=>{
               if (pinnedView)
                 this.unpinTicket(ticket)
               else
